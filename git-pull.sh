2cf2bb1079c1f2f26eb43123cb3f0774662d899c
Ryan Hilliard <ryanhhilliard@gmail.com>
2026-03-06 06:01:43 -0500

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -5,5 +5,5 @@ rm -rf frontend/dist
 
 git pull
 
-sudo chown -R nginx:nginx /home/ec2-user/4155-project/frontend/dist
-sudo chmod -R 755 /home/ec2-user/4155-project/frontend/dist
+sudo chown -R ec2-user:nginx /home/ec2-user/4155-project/frontend/dist
+sudo chmod -R 775 /home/ec2-user/4155-project/frontend/dist
